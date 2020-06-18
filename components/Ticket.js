26b3d39b411e425bf23273c6a3f644cd6a81c077
cknoettg <cknoettg@yahoo.com>
2020-06-18 03:26:29 -0400

    Built out Ticket component

--- components/Ticket.js
+++ components/Ticket.js
@@ -1,7 +1,7 @@
 //To create a React component, we use ICE:
 //  Import - Component - Export (with default)
 import React, { useState, useEffect } from "react";
-
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 // *** This file creates a Ticket component ***
 //   the properties/state for each individual ticket
 //     are going to be passed up to TicketList
@@ -9,7 +9,9 @@ import React, { useState, useEffect } from "react";
 //                  many tickets
 //   TicketList is a parent component that is going
 //    to hold all our child Tickets from this file
-const Ticket = () => {
+//   In another context, Ticket is the grandchild,
+//     TicketList is the child, and App is the parent
+const Ticket = props => {
     //this is our state variable
     const [ticket, setTicket] = useState([]);
     //what properties does ticket contain? :
@@ -29,6 +31,7 @@ const Ticket = () => {
     // UseEffect is a React hook that lets us perform
     //   an action after a render takes place
     // In this case, it allows us to fetch data
+    //   fetching data is an example of a side effect
     // useEffect(() => {
     //     const generateBubbleData = colors.map((_, i) => ({
     //       value: Math.floor(Math.random() * (colors.length * 2)) + 1,
@@ -37,16 +40,75 @@ const Ticket = () => {
     //     setBubbleData(generateBubbleData);
     //   }, [colors]);
 
+    //Change Handler
+    const handleChange = e => {
+        setName(e.target.value)
+    }
     //What actions do we want to perform on ticket?
+    //    (functions)
     //  create ticket - defined in BackEnd /data/dbaccess.js
     //  assign ticket - ""
     //  close or reopen ticket - ""
     
+    // create ticket is going to involve an axios.post request
+    
+    // assign ticket is going to be an axios.put request
+    
+    // close/reopen ticket is going to be an axios.put request
+    
+    // function allowing us to toggle our dropdown
+    //   TODO: add prevState to our state
+    const toggle = () => setDropdownOpen(prevState => !prevState);
     // render function is required for React components
     //   it uses JSX, which is HTML-like JavaScript
     return (
         <div className="ticket-class">
             <h3>I am a ticket!</h3>
+            <form>
+                {/* display id */}
+                <p>Ticket ID: {props.id}</p>
+                {/* display and add title */}
+                <input type="text" 
+                value={props.title} 
+                onChange={handleChange}
+                placeholder="It's Not Working!!"    
+                />
+                {/* display description */}
+                <input type="textarea" 
+                value={props.description} 
+                onChange={handleChange}    
+                />
+                {/* display what user has "tried" */}
+                <input type="textarea" 
+                value={props.tried} 
+                onChange={handleChange}    
+                />
+                {/* Dropdowns are reactstrap components*/}
+                {/* https://reactstrap.github.io/components/dropdowns/ */}
+                {/* display ticket "category" */}
+                <Dropdown toggle={toggle} />
+                    <DropdownToggle caret>
+                        Category
+                        </DropdownToggle>
+
+                    <DropdownMenu>
+                        <DropdownItem>Low</DropdownItem>
+                        <DropdownItem>Medium</DropdownItem>
+                        <DropdownItem>High</DropdownItem>
+                        <DropdownItem>Critical</DropdownItem>
+                    </DropdownMenu>
+                {/* display ticket "status" */}
+                <Dropdown toggle={toggle} />
+                    <DropdownToggle caret>
+                        Status
+                        </DropdownToggle>
+
+                    <DropdownMenu>
+                        <DropdownItem>Open</DropdownItem>
+                        <DropdownItem>Reopen</DropdownItem>
+                        <DropdownItem>Closed</DropdownItem>
+                    </DropdownMenu>
+            </form>
         </div>
     )
 };

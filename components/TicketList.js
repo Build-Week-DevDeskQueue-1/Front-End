dc2ead2d013a3e67a6e175a8dbc0f4cc2a63a79c
cknoettg <cknoettg@yahoo.com>
2020-06-17 21:53:57 -0400

    Created Mock Ticket and TicketList, axiosWithAuth skeleton.

---
+++ components/TicketList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect }  from 'react';
+import Ticket from './Ticket';
+//this is going to be the component
+//  to display all tickets
+
+//the /ticket <Route> in App.js will link 
+//  to this page
+const TicketList = () => {
+    const[tickets,setTickets]= useState([]);
+
+    useEffect(() =>{
+        axiosWithAuth()
+        .get("/tickets") //.get('http://localhost:5000/api/tickets')
+        .then(res => setTickets(res.data))
+        .catch(err => console.log(err.response));
+      }, []);
+
+      return (
+        <>
+        <Ticket />
+          {/* <ColorList colors={colorList} updateColors={setColorList} />
+          <Bubbles colors={colorList} /> */}
+        </>
+      );
+}
+
+export default TicketList;
--- components/Tickets.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-//this is going to be the component
-//  to display all tickets
-
-//the /ticket <Route> in App.js will link 
-//  to this page

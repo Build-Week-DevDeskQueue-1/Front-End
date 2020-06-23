492cde1057ddde5f079608dc20a5b67466e4be38
cknoettg <cknoettg@yahoo.com>
2020-06-22 23:56:42 -0400

    fixed missing import

--- login/src/App.js
+++ login/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import <BrowserRouter as Router, Route> from 'react-router-dom';
-import Axios from 'axios'
-import { Router } from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
 

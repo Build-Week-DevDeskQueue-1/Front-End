a67bcb413392fc879735df91b93422aef016680b
cknoettg <cknoettg@yahoo.com>
2020-06-22 23:54:48 -0400

    Attempted to reinstall missing dependencies

--- login/src/App.js
+++ login/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import <BrowserRouter as Router, Route> from 'react-router-dom';
 import Axios from 'axios'
+import { Router } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
+      <h1>DevDesk Ticketing System</h1>
         {/*
          
          

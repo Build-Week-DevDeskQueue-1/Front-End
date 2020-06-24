df409e2365c68f7d58d3913d97845112663e5c6e
cknoettg <cknoettg@yahoo.com>
2020-06-24 00:46:07 -0400

    Added tickets and registration to App

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login';
+import Registration from "./components/Registration";
 import PrivateRoute from "./components/PrivateRoute";
 import Ticket from "./components/Ticket";
 
@@ -12,7 +13,7 @@ function App() {
       <h1>DevDesk Ticketing System</h1>
         <Router>
           <Route exact path="/" component={Login} />
-       
+          <Route path="/register" component={Registration} />
           <PrivateRoute path="/ticket" component={Ticket}
           render={props=>(
             <Ticket {...props} />

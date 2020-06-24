715eba421e95c008b2eb7081cc09a96d45494179
cknoettg <cknoettg@yahoo.com>
2020-06-23 22:05:27 -0400

    Reinstall and Recreate Phase IV

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -1,4 +1,4 @@
-;import React from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login';

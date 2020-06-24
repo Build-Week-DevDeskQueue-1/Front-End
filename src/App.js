f7afb051a64f0dbde553d9a697c791c769ca482c
cknoettg <cknoettg@yahoo.com>
2020-06-23 21:16:55 -0400

    Added login, route to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import axios from 'axios';
+import Login from './Login';
 
 function App() {
 
   return (
     <div className="App">
       <h1>DevDesk Ticketing System</h1>
+        <Router>
+          <Route exact path="/" component={Login} />
+        </Router>
+        
         {/*
          
          

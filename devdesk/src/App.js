47a213c265157caead8ee7625e380a812190b04a
cknoettg <cknoettg@yahoo.com>
2020-06-25 00:54:09 -0400

    Installed Less, Added Initial Styling to App.js

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './css/index.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Helmet} from "react-helmet";
 import Login from './components/Login';
@@ -7,7 +8,7 @@ import Logout from './components/Logout';
 import Registration from "./components/Registration";
 import PrivateRoute from "./components/PrivateRoute";
 import Ticket from "./components/Ticket";
-//Redeploy
+
 function App() {
 
   return (
@@ -15,6 +16,8 @@ function App() {
       <Helmet>
         <title>DevDesk Ticketing System</title>
       </Helmet>
+      {/* LOGO */}
+      <a href='https://lambdaschool.com/' target="_blank" class="logo"><img src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5dcda59e63bb6ae5c9282801_small-red-logo.png" alt="Lambda school logo" width="250px" /></a>
       <h1>DevDesk Ticketing System</h1>
         <Router>
           <Route exact path="/" component={Login} />

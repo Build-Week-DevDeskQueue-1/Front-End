dd8edd200da2d74679fc49461f6ca7fc776fc584
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-06-25 23:58:28 -0400

    example fixes

---
+++ devdesk/src/components/Login - Copy.js
@@ -0,0 +1,75 @@
+import React,{useState} from 'react';
+import {BrowserRouter as Router, Link, useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+
+const Login = () => {
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
+    const [credentials, setCredentials] = useState({ username: '', password: '' });
+    const history = useHistory();
+
+    //change handler
+    const handleChange = e => {
+        setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+        })
+    }
+
+    const logins = e => {
+        e.preventDefault();
+        axios
+          .post(`https://dev-desk-backend.herokuapp.com/auth/login`, credentials) ///api/login
+          .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            history.push('/ticket');
+          })
+          .catch(err => console.log(err));
+        setCredentials({ username: '', password: '' });
+      }
+    
+    //function to refresh page
+    function refreshPage(){ 
+      window.location.reload(); 
+    }
+
+    return (
+        <form className="loginForm" onSubmit={logins}>
+            <div className="labels">
+            <label>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    value={credentials.username}/>
+            </label>            
+            <label>
+                <input
+                    type="text"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    value={credentials.password}/>
+            </label> 
+            </div>     
+            <div className="buttons">
+              <button type='submit' onClick={ refreshPage }>
+                <Router>
+                      <Link to="/ticket">Login</Link>
+                </Router>
+              </button> 
+              <button onClick={ refreshPage }>
+              <Router>
+                <Link to="/register">Register</Link>
+              </Router>
+              </button>  
+            </div>   
+        </form>
+    )
+
+}
+
+export default Login;
---
+++ devdesk/src/components/Registration - Copy.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Registration extends React.Component{
+    //this is the initial state
+    state = {
+        newUser: {
+            username:'',
+            password:'',
+            is_student:false,
+            is_helper:false
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            newUser: {
+                ...this.state.newUser,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    addUser = e => {
+        e.preventDefault();
+        
+        axios
+        .post(`https://dev-desk-backend.herokuapp.com/auth/register`, this.state.newUser)
+        .then(res => {
+            console.log(res.data);
+            //TODO: this will not be friends, but something else
+            //this.setState({friends:[...res.data, res.data.payload]});
+            this.setState({...res.data});
+        })
+        .catch(err => console.log(err));
+
+        //TODO: Add logic to clear form on submit
+
+        //TODO: Add logic - if helper is checked
+        //  redirect to TicketList
+        // -if student is checked - redirect to
+        //  Your Ticket Has Been Submitted Page
+        // - disallow both to be checked
+    };
+
+    //function to refresh page
+    refreshPage(){ 
+        window.location.reload(); 
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h3>Registration Form</h3>
+                <form className="registerForm" onSubmit={this.addUser}>
+                    <label>
+                        <input type="text" name="username" placeholder="Username" value={this.state.newUser.username} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                    <input type="text" name="password" placeholder="Password" value={this.state.newUser.password} onChange={this.handleChange}/>
+                    </label>
+                    
+                    <label>
+                        Are You a Student?
+                        {/* TODO: Toggle true and false*/}
+                        <input
+                        type="checkbox"
+                        name="isStudent"
+                        onChange={this.handleChange}
+                        value={this.state.newUser.is_student}/>
+                    </label>
+
+                    <label>
+                        Are You a Helper?
+                        {/* TODO: Toggle true and false*/}
+                        <input
+                        type="checkbox"
+                        name="isHelper"
+                        onChange={this.handleChange}
+                        value={this.state.newUser.is_helper}/>
+                    </label>
+
+                    <button type="submit" onClick={this.refreshPage}>
+                        <Router>
+                            <Link to="/ticket">Register</Link>
+                        </Router>
+                    </button>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Registration;
